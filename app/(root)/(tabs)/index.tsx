cdc78902e6ad7eea841323efcf58631429d442c5
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-02-12 20:18:11 +0100

    chore: remove dummy page content :zap:

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -11,13 +11,7 @@ export default function Index() {
       }}
     >
       <Text className="font-bold text-3xl text-red-800">Welcome to the app </Text>
-      {/* create routes */}
-      <Link href="/properties/1">Property 1</Link>
-      <Link href="/properties/2">Property 2</Link>
-      <Link href="/properties/3">Property 3</Link>
-      <Link href="/sign-in">Sign In</Link>
-      <Link href="/profile">Profile</Link>
-      <Link href="/explore">Explore</Link>
+
     </View>
   );
 }
