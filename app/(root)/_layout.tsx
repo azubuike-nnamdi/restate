55b3563c9cd9fd76986fa60c8fe873ed1cd07886
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-02-13 18:38:22 +0100

    refactor: layout :hammer:

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -1,23 +1,26 @@
 import { SIGN_IN_URL } from "@/config/routes";
-import { GlobalProvider, useGlobalContext } from "@/lib/global-provider";
-import { Redirect } from "expo-router";
+import { useGlobalContext } from "@/lib/global-provider";
+import { Redirect, Slot } from "expo-router";
 import { ActivityIndicator } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Slot } from "expo-router";
 
 export default function AppLayout() {
-  //get access to loading and isLoggedIn state from  global provider
   const { loading, isLoggedIn } = useGlobalContext();
+
+  // Add debug logging
+  console.log("AppLayout State:", { loading, isLoggedIn });
+
+  if (!isLoggedIn && !loading) {
+    return <Redirect href={SIGN_IN_URL} />;
+  }
 
   if (loading) {
     return (
       <SafeAreaView className="bg-white h-full flex justify-center items-center">
         <ActivityIndicator size="large" className="text-primary-300" />
       </SafeAreaView>
-    )
+    );
   }
 
-  if (!isLoggedIn) return <Redirect href={SIGN_IN_URL} />
-
-  return <Slot />
+  return <Slot />;
 }
